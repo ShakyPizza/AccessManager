bc80c3d49b64098431d4b0c98aede83c620e4fe7
ShakyPizza <105827753+ShakyPizza@users.noreply.github.com>
2025-08-03 22:59:15 +0000

    added real doorlist

--- docs/src/data/Doorlist.js
+++ docs/src/data/Doorlist.js
@@ -1,12 +1,293 @@
 export const doors = [
   {
-    value: 'main-entrance',
-    label: 'Main Entrance',
-    groups: ['a', 'b'],
+    value: 'tengivirki-nordan',
+    label: 'Tengivirki norðan',
+    groups: ['a'],
   },
   {
-    value: 'back-door',
-    label: 'Back Door',
+    value: 'haspennuherbergi-1',
+    label: 'Háspennuherbergi 1',
+    groups: ['a'],
+  },
+  {
+    value: 'haspennuherbergi-2',
+    label: 'Háspennuherbergi 2',
+    groups: ['a'],
+  },
+  {
+    value: 'askja-taeknirim',
+    label: 'Askja tæknirými',
+    groups: ['a'],
+  },
+  {
+    value: 'thr-11kv-0',
+    label: 'THR - 11KV 0',
+    groups: ['a'],
+  },
+  {
+    value: 'thr-11kv-1',
+    label: 'THR - 11KV 1',
+    groups: ['a'],
+  },
+  {
+    value: 'thr-11kv-2',
+    label: 'THR - 11KV 2',
+    groups: ['a'],
+  },
+  {
+    value: 'thr-velaspennir-1',
+    label: 'THR - Vélaspennir 1',
+    groups: ['a'],
+  },
+  {
+    value: 'thr-velaspennir-2',
+    label: 'THR - Vélaspennir 2',
+    groups: ['a'],
+  },
+  {
+    value: 'rafmagnsherbergi-1',
+    label: 'Rafmagnsherbergi 1',
+    groups: ['b'],
+  },
+  {
+    value: 'rafmagnsherbergi-2',
+    label: 'Rafmagnsherbergi 2',
+    groups: ['b'],
+  },
+  {
+    value: 'rafmagnsherbergi-3-austan',
+    label: 'Rafmagnsherbergi 3 austan',
+    groups: ['b'],
+  },
+  {
+    value: 'rafmangsherbergi-3-vestan',
+    label: 'Rafmangsherbergi 3 vestan',
+    groups: ['b'],
+  },
+  {
+    value: 'gestastofa-velasalur',
+    label: 'Gestastofa/vélasalur',
+    groups: ['b'],
+  },
+  {
+    value: 'rafmangsverkstaedi',
+    label: 'Rafmangsverkstæði',
+    groups: ['b'],
+  },
+  {
+    value: 'rannsokn-vestan',
+    label: 'Rannsókn vestan',
+    groups: ['b'],
+  },
+  {
+    value: 'rannsokn-austan',
+    label: 'Rannsókn austan',
+    groups: ['b'],
+  },
+  {
+    value: 'stjornherbergi-vestan',
+    label: 'Stjórnherbergi vestan',
+    groups: ['b'],
+  },
+  {
+    value: 'stjornherbergi-austan',
+    label: 'Stjórnherbergi austan',
+    groups: ['b'],
+  },
+  {
+    value: 'stigahus-austan-3h-velasalur',
+    label: 'Stigahús austan 3h/vélasalur',
+    groups: ['b'],
+  },
+  {
+    value: 'stodvarhus-hringstigi',
+    label: 'Stöðvarhús hringstigi',
+    groups: ['b'],
+  },
+  {
+    value: 'skemma-1-baldursheimur',
+    label: 'Skemma 1 Baldursheimur',
+    groups: ['b'],
+  },
+  {
+    value: 'askja-starfsmannarymi',
+    label: 'Askja, inn í starfsmannarými',
+    groups: ['b'],
+  },
+  {
+    value: 'askja-skrifstofurymi',
+    label: 'Askja, inn á skrifstofurými',
+    groups: ['b'],
+  },
+  {
+    value: 'askja-ingangur',
+    label: 'Askja, skrifstofur inngangur',
+    groups: ['b'],
+  },
+  {
+    value: 'askja-stjornherbergi',
+    label: 'Askja, stjórnherbergi',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-inntaksrymi',
+    label: 'THR - inntaksrými',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-millistuka',
+    label: 'THR - millistúka',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-millihurd-gangur-1h',
+    label: 'THR - millihurð gangur 1.h',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-grofv-mannvist',
+    label: 'THR - grófv./mannvist',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-grofv-fatakompa',
+    label: 'THR - grófv./fatakompa',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-vaktir-og-varnir-1',
+    label: 'THR - vaktir og varnir 1',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-400v-1',
+    label: 'THR - 400V 1',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-vaktir-og-varnir-2',
+    label: 'THR - vaktir og varnir 2',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-400v-2',
+    label: 'THR - 400V 2',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-millihurd-gangur-2h',
+    label: 'THR - millihurð gangur 2.h',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-sameiginlegur-rafbunadur',
+    label: 'THR - sameiginlegur rafbún.',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-rafbunadur-fjarskipti-a',
+    label: 'THR - Rafbún. og fjarskipti A',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-rafbunadur-fjarskipti-n',
+    label: 'THR - Rafbún. og fjarskipti N',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-stjornherbergi',
+    label: 'THR - stjórnherbergi',
+    groups: ['b'],
+  },
+  {
+    value: 'thr-nidurrennslishus',
+    label: 'THR - niðurrennslishús',
+    groups: ['b'],
+  },
+  {
+    value: 'stodvarhus-nordur',
+    label: 'Stöðvarhús norður',
+    groups: ['c'],
+  },
+  {
+    value: 'stodvarhus-austur',
+    label: 'Stöðvarhús austur',
+    groups: ['c'],
+  },
+  {
+    value: 'skemma-1-inngangur',
+    label: 'Skemma 1 inngangur',
+    groups: ['c'],
+  },
+  {
+    value: 'skemma-2-inngangur',
+    label: 'Skemma 2 inngangur',
+    groups: ['c'],
+  },
+  {
+    value: 'askja-adalingangur',
+    label: 'Askja aðalinngangur',
+    groups: ['c'],
+  },
+  {
+    value: 'dyngja-adalingangur',
+    label: 'Dyngja aðalinngangur',
+    groups: ['c'],
+  },
+  {
+    value: 'gufustodin-inngangur',
+    label: 'Gufustöðin inngangur',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-adalingangur-stod',
+    label: 'THR - aðalinngangur stöð',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-grofvinnuverkst-n',
+    label: 'THR - grófvinnuverkst. N',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-grofvinnuverkst-a',
+    label: 'THR - grófvinnuverkst. A',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-grofvinnuverkst-velasalur',
+    label: 'THR - grófvinnuverkst/vélasalur',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-sprinklerrými',
+    label: 'THR - útihurð sprinklerrými',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-velasal-a-1',
+    label: 'THR - útihurð vélasal A',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-velasal-a-2',
+    label: 'THR - útihurð vélasal A',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-velasal-s',
+    label: 'THR - útihurð vélasal S',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-vel-2-v',
+    label: 'THR - útihurð vél 2 V',
+    groups: ['c'],
+  },
+  {
+    value: 'thr-utihurd-vel-1-v',
+    label: 'THR - útihurð vél 1 V',
     groups: ['c'],
   },
 ];
+
